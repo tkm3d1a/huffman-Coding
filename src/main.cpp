f41ecb9eb913338d6abbb6d0c10ca9c6d456e89d
Tim Klimpel <tim.klimpel@gmail.com>
2022-07-20 07:01:02 -0400

    Reorganized and setup folder structure

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+* Author: Tim Klimpel
+* Assignment Title: 
+* Assignment Description: 
+* Due Date: 8/2/2022
+* Date Created: 7/20/2022
+* Date Last Modified: 7/20/2022
+*/
+
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::cout << "Hello world test" << std::endl;
+}
